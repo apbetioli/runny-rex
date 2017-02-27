d31831c1b428579b24cc5188842ee0aad831e59f
Alexandre Parra Betioli <apbetioli@gmail.com>
2017-02-27 17:19:38 -0300

    Initial commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent (typeof(Rigidbody2D))]
+[RequireComponent (typeof(Collider2D))]
+public class Player : MonoBehaviour
+{
+	public float jumpHeight = 20;
+	public float timeToJumpApex = .4f;
+
+	private Rigidbody2D body;
+	private bool onTheGround = false;
+	private float gravity;
+	private float jumpVelocity;
+
+	void Awake ()
+	{
+		body = GetComponent<Rigidbody2D> ();
+	}
+
+	void Start ()
+	{
+		CalculateGravity ();
+		AdjustJump ();
+	}
+
+	void Update ()
+	{
+		if (Input.GetButtonDown ("Jump") || Input.GetKeyDown (KeyCode.UpArrow))
+			Jump ();
+		else if (Input.GetKeyDown (KeyCode.DownArrow))
+			Duck ();
+
+		CalculateGravity ();
+		AdjustJump ();
+	}
+
+	private void CalculateGravity ()
+	{
+		gravity = -(2 * jumpHeight) / Mathf.Pow (timeToJumpApex, 2);
+
+		float velocityY = body.velocity.y + gravity * Time.deltaTime;
+		SetVelocityY (velocityY);
+	}
+
+	private void AdjustJump ()
+	{
+		jumpVelocity = Mathf.Abs (gravity) * timeToJumpApex;
+	}
+
+	private void SetVelocityY (float velocityY)
+	{
+		Vector2 velocity = body.velocity;
+		velocity.y = velocityY;
+		body.velocity = velocity;
+	}
+
+	public void Jump ()
+	{
+		if (!onTheGround)
+			return;
+		
+		SetVelocityY (jumpVelocity);
+		onTheGround = false;
+	}
+
+	public void Duck ()
+	{
+		if (!onTheGround)
+			return;
+
+		//TODO
+	}
+
+	void OnCollisionEnter2D (Collision2D other)
+	{
+		if ("Ground" == other.gameObject.tag) {
+			SetVelocityY (0);
+			onTheGround = true;
+		}
+	}
+
+}
---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIController : MonoBehaviour
+{
+	private Player player;
+
+	void Awake ()
+	{
+		player = FindObjectOfType<Player> ();
+	}
+
+	public void Duck ()
+	{
+		player.Duck ();
+	}
+
+	public void Jump ()
+	{
+		player.Jump ();
+	}
+}
