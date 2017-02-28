0ff47f7d39644a65c590a7b20e9a2d78e0bd5321
Alexandre Parra Betioli <apbetioli@gmail.com>
2017-02-28 18:23:40 -0300

    Game Over

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -3,117 +3,125 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-[RequireComponent(typeof(Rigidbody2D))]
-[RequireComponent(typeof(Collider2D))]
+[RequireComponent (typeof(Rigidbody2D))]
+[RequireComponent (typeof(Collider2D))]
 public class Character : MonoBehaviour
 {
-    public float maxJumpHeight = 40;
-    public float minJumpHeight = 10;
-    public float timeToJumpApex = .4f;
+	public float maxJumpHeight = 40;
+	public float minJumpHeight = 10;
+	public float timeToJumpApex = .4f;
 
-    private Rigidbody2D body;
-    private bool onTheGround = false;
-    private bool duck = false;
+	private Rigidbody2D body;
+	private bool onTheGround = false;
+	private bool duck = false;
 	private bool dead = false;
-    private float gravity;
-    private float maxJumpVelocity;
-    private float minJumpVelocity;
+	private float gravity;
+	private float maxJumpVelocity;
+	private float minJumpVelocity;
+	private Animator animator;
 
-    void Awake()
-    {
-        body = GetComponent<Rigidbody2D>();
-    }
+	void Awake ()
+	{
+		body = GetComponent<Rigidbody2D> ();
+		animator = GetComponentInChildren<Animator> ();
+		dead = false;
+	}
 
-    void Start()
-    {
-        CalculateGravity();
-        AdjustJump();
-    }
+	void Start ()
+	{
+		CalculateGravity ();
+		AdjustJump ();
+	}
 
-    void Update()
-    {
+	void Update ()
+	{
+		animator.SetBool ("Ground", onTheGround);
+		animator.SetBool ("Duck", duck);
+		animator.SetBool ("Dead", dead);
 
-        duck = false;
-		if (IsJumpPressed())
-            Jump();
-		else if (IsJumpReleased())
-            ReleaseJump();
-		else if (IsDuckPressed())
-            Duck();
+		if (dead) {
+			SetVelocityY (0);
+			return;
+		}
+		
+		duck = false;
 
-        GetComponentInChildren<Animator>().SetBool("Ground", onTheGround);
-        GetComponentInChildren<Animator>().SetBool("Duck", duck);
-		GetComponentInChildren<Animator>().SetBool("Dead", dead);	
+		if (IsJumpPressed ())
+			Jump ();
+		else if (IsJumpReleased ())
+			ReleaseJump ();
+		else if (IsDuckPressed ())
+			Duck ();
+		
+		CalculateGravity ();
+		AdjustJump ();
+	}
 
-        CalculateGravity();
-        AdjustJump();
-    }
-
-	private bool IsJumpPressed() {
+	private bool IsJumpPressed ()
+	{
 		return Input.GetButton ("Jump") || Input.GetKey (KeyCode.UpArrow);
 	}
 
-	private bool IsJumpReleased() {
+	private bool IsJumpReleased ()
+	{
 		return Input.GetButtonUp ("Jump") || Input.GetKeyUp (KeyCode.UpArrow);
 	}
 
-	private bool IsDuckPressed() {
+	private bool IsDuckPressed ()
+	{
 		return Input.GetKey (KeyCode.DownArrow);
 	}
 
-    private void CalculateGravity()
-    {
-        gravity = -(2 * maxJumpHeight) / Mathf.Pow(timeToJumpApex, 2);
+	private void CalculateGravity ()
+	{
+		gravity = -(2 * maxJumpHeight) / Mathf.Pow (timeToJumpApex, 2);
 
-        float velocityY = body.velocity.y + gravity * Time.deltaTime;
-        SetVelocityY(velocityY);
-    }
+		float velocityY = body.velocity.y + gravity * Time.deltaTime;
+		SetVelocityY (velocityY);
+	}
 
-    private void AdjustJump()
-    {
-        maxJumpVelocity = Mathf.Abs(gravity) * timeToJumpApex;
-        minJumpVelocity = Mathf.Sqrt(2 * Mathf.Abs(gravity) * minJumpHeight);
-    }
+	private void AdjustJump ()
+	{
+		maxJumpVelocity = Mathf.Abs (gravity) * timeToJumpApex;
+		minJumpVelocity = Mathf.Sqrt (2 * Mathf.Abs (gravity) * minJumpHeight);
+	}
 
-    private void SetVelocityY(float velocityY)
-    {
-        Vector2 velocity = body.velocity;
-        velocity.y = velocityY;
-        body.velocity = velocity;
-    }
+	private void SetVelocityY (float velocityY)
+	{
+		body.velocity = new Vector2(0, velocityY);
+	}
 
-    public void Jump()
-    {
-        if (!onTheGround)
-            return;
+	public void Jump ()
+	{
+		if (!onTheGround)
+			return;
 
-        SetVelocityY(maxJumpVelocity);
-        onTheGround = false;
-    }
+		SetVelocityY (maxJumpVelocity);
+		onTheGround = false;
+	}
 
-    public void ReleaseJump()
-    {
-        if (body.velocity.y > minJumpVelocity)
-        {
-            SetVelocityY(minJumpVelocity);
-        }
-    }
+	public void ReleaseJump ()
+	{
+		if (body.velocity.y > minJumpVelocity) {
+			SetVelocityY (minJumpVelocity);
+		}
+	}
 
-    public void Duck()
-    {
-        if (!onTheGround)
-            return;
-        duck = true;
-        //TODO
-    }
+	public void Duck ()
+	{
+		if (!onTheGround)
+			return;
+		duck = true;
+	}
 
 	void OnCollisionEnter2D (Collision2D other)
 	{
 		if ("Ground" == other.gameObject.tag) {
 			SetVelocityY (0);
 			onTheGround = true;
+
 		} else if ("Enemy" == other.gameObject.tag) {
-			GameManager.instance.Die ();
+			GameManager.Die ();
 			dead = true;
 		}
 	}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameManager : MonoBehaviour
+{
+	public float speed = 200;
+
+	public static GameManager instance;
+
+	void OnEnable ()
+	{
+		GameManager.instance = this;
+	}
+
+	void Awake ()
+	{
+		DontDestroyOnLoad (this);
+		Time.timeScale = 1;
+		UI ();
+	}
+
+	public static void Die ()
+	{
+		Time.timeScale = 0;
+		SceneManager.LoadScene ("GameOver", LoadSceneMode.Additive);
+	}
+
+	public static void Restart ()
+	{
+		SceneManager.LoadScene ("Run");
+	}
+
+	public static void UI ()
+	{
+		SceneManager.LoadScene ("UI", LoadSceneMode.Additive);
+	}
+}
