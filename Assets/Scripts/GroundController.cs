195198264a86452bc30d50be695052a41150e6ff
Alexandre Parra Betioli <apbetioli@gmail.com>
2017-02-28 01:08:02 -0300

    Geração do cenário

---
+++ Assets/Scripts/GroundController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundController : MonoBehaviour {
+
+	public float velocity = 5f;
+
+	public GameObject groundPrefab;
+	public float groundSize = 500f;
+	public float groundCount = 2;
+	private Queue<GameObject> groundPool = new Queue<GameObject>();
+
+	void Start () {
+		CreateGround ();
+	}
+	
+	void Update () {
+		UpdateGroundPosition ();
+	}
+
+	private void CreateGround() {
+		float x = 0;
+		for (int i = 0; i < groundCount; i++) {
+			GameObject ground = Instantiate (groundPrefab);
+			ground.transform.parent = transform;
+			ground.transform.position = new Vector3 (x, 0, 0);
+			groundPool.Enqueue (ground);
+			x += groundSize;
+		}
+	}
+
+	private void UpdateGroundPosition() {
+		foreach(GameObject obj in groundPool) {
+			obj.transform.position += Vector3.left * velocity * Time.deltaTime;
+		}
+
+		if (groundPool.Peek ().transform.position.x < -groundSize) {
+			GameObject old = groundPool.Dequeue ();
+			Vector3 position = old.transform.position;
+			position.x = groundSize * (groundCount -1);
+			old.transform.position = position;
+			groundPool.Enqueue (old);
+		}
+	}
+}
---
+++ Assets/Scripts/ScenarioElementController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScenarioElementController : MonoBehaviour {
+
+	public ScenarioElement[] prefabs;
+	public Transform spawnPoint;
+	public Transform destroyPoint;
+	public float velocity = 40f;
+	public float minTime = 1f;
+	public float maxTime = 4f;
+
+	private float time = 0f;
+	private float sumTime = 0f;
+	private Queue<GameObject> pool = new Queue<GameObject> ();
+
+	void Start () {
+		time = RandomTime();
+	}
+	
+	void Update () {
+		CreateElements();
+	}
+
+	private void CreateElements() {
+		if (sumTime < time)
+			sumTime += Time.deltaTime;
+		else {
+			time = RandomTime();
+			sumTime = 0f;
+
+			GameObject obj = Spawn ();
+			pool.Enqueue (obj);
+		}
+
+		if(pool.Count > 0 && pool.Peek ().transform.position.x < destroyPoint.position.x) {
+			GameObject old = pool.Dequeue ();
+			Destroy (old);
+		}
+
+		foreach(GameObject obj in pool) {
+			obj.transform.position += Vector3.left * velocity * Time.deltaTime;
+		}
+	}
+
+	private GameObject Spawn() {
+		int index = Random.Range (0, prefabs.Length);
+		ScenarioElement element = prefabs [index];
+		float spawnPositionY = Random.Range (element.minY, element.maxY);
+
+		GameObject obj = Instantiate (element.gameObject);
+		obj.transform.parent = transform;
+		obj.transform.position = new Vector3(spawnPoint.position.x, spawnPositionY, 0);
+		return obj;
+	}
+
+	private float RandomTime() {
+		return Random.Range (minTime, maxTime);
+	}
+
+
+}
